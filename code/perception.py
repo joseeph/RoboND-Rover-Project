55679e40365594c18c2c3ed8bb95d6343b335480
Joseph Wang <Joseph@Jo.local>
2017-06-01 05:45:08 +0800

    edited

--- code/perception.py
+++ code/perception.py
@@ -96,28 +96,7 @@ def perspect_transform(img, src, dst):
 
 # Apply the above functions in succession and update the Rover state accordingly
 def perception_step(Rover):
-    # Perform perception steps to update Rover()
-    # TODO: 
-    # NOTE: camera image is coming to you in Rover.img
-    # 1) Define source and destination points for perspective transform
-    # 2) Apply perspective transform
-    # 3) Apply color threshold to identify navigable terrain/obstacles/rock samples
-    # 4) Update Rover.vision_image (this will be displayed on left side of screen)
-    # Example: Rover.vision_image[:,:,0] = obstacle color-thresholded binary image
-    #          Rover.vision_image[:,:,1] = rock_sample color-thresholded binary image
-    #          Rover.vision_image[:,:,2] = navigable terrain color-thresholded binary image
-
-    # 5) Convert map image pixel values to rover-centric coords
-    # 6) Convert rover-centric pixel values to world coordinates
-    # 7) Update Rover worldmap (to be displayed on right side of screen)
-    # Example: Rover.worldmap[obstacle_y_world, obstacle_x_world, 0] += 1
-    #          Rover.worldmap[rock_y_world, rock_x_world, 1] += 1
-    #          Rover.worldmap[navigable_y_world, navigable_x_world, 2] += 1
-
-    # 8) Convert rover-centric pixel positions to polar coordinates
-    # Update Rover pixel distances and angles
-    # Rover.nav_dists = rover_centric_pixel_distances
-    # Rover.nav_angles = rover_centric_angles
+    
 
    
     # Perform perception steps to update Rover()
@@ -144,9 +123,9 @@ def perception_step(Rover):
             # Example: Rover.vision_image[:,:,0] = obstacle color-thresholded binary image
             #          Rover.vision_image[:,:,1] = rock_sample color-thresholded binary image
             #          Rover.vision_image[:,:,2] = navigable terrain color-thresholded binary image
-    Rover.vision_image[:,:,0] = obstacles_warped_threshed 
-    Rover.vision_image[:,:,1] = rock_warped_threshed
-    Rover.vision_image[:,:,2] = terrain_warped_threshed
+    Rover.vision_image[:,:,0] = obstacles_warped_threshed *255
+    Rover.vision_image[:,:,1] = rock_warped_threshed*255
+    Rover.vision_image[:,:,2] = terrain_warped_threshed*255
 
 
 
